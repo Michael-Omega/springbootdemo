af7a44312a63d2594bca41f3f60d3934756c76ae
吴雄 <wuxiong_sc@163.com>
2022-06-30 09:45:23 +0800

    2022年修改项目结构

--- src/main/java/com/project/config/RedisConfig.java
+++ src/main/java/com/project/config/RedisConfig.java
@@ -1,41 +1,57 @@
-//package com.project.config;
-//
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.redis.connection.RedisConnectionFactory;
-//import org.springframework.data.redis.core.RedisTemplate;
-//import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-//import org.springframework.data.redis.serializer.StringRedisSerializer;
-//import com.fasterxml.jackson.annotation.JsonAutoDetect;
-//import com.fasterxml.jackson.annotation.PropertyAccessor;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//
-//
-//@Configuration
-//public class RedisConfig {
-//
-//    @Bean
-//    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
-//        RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
-//        template.setConnectionFactory(factory);
-//        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
-//        ObjectMapper om = new ObjectMapper();
-//        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-//        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-//        jackson2JsonRedisSerializer.setObjectMapper(om);
-//        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
-//        // key采用String的序列化方式
-//        template.setKeySerializer(stringRedisSerializer);
-//        // hash的key也采用String的序列化方式
-//        template.setHashKeySerializer(stringRedisSerializer);
-//        // value序列化方式采用jackson
-//        template.setValueSerializer(jackson2JsonRedisSerializer);
-//        // hash的value序列化方式采用jackson
-//        template.setHashValueSerializer(jackson2JsonRedisSerializer);
-//        template.afterPropertiesSet();
-//        return template;
-//
-//    }
-//
-//}
+package com.project.config;
+
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    @SuppressWarnings("all")
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+
+        // Json序列化配置
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        // enableDefaultTyping 方法已经过时，使用新的方法activateDefaultTyping
+        // objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.WRAPPER_ARRAY);
+        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+
+        // String序列化
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+
+        // key 序列化方式
+        template.setKeySerializer(stringRedisSerializer);
+
+        // hash的key序列化方式
+        template.setHashKeySerializer(stringRedisSerializer);
+
+        // value的序列化方式（json序列化）
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+        // value的序列化方式（String序列化，value 不是String类型会报错，建议使用json序列化）
+        // template.setValueSerializer(stringRedisSerializer);
+
+        // hash的value序列化方式（json序列化）
+        template.setHashValueSerializer(jackson2JsonRedisSerializer);
+
+        template.afterPropertiesSet();
+        return template;
+
+    }
+
+}
